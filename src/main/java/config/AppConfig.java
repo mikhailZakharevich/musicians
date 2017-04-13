8aa7cf0362d8ba20033dc541888fbac003aff445
TanyaOdyniuk <tanyaOdyniuk@gmail.com>
2017-04-13 13:47:59 +0300

    Added Composition controller and jsps without bootstrap

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -29,7 +29,7 @@ public class AppConfig {
         dataSource.setDriverClassName("com.mysql.jdbc.Driver");
         dataSource.setUrl("jdbc:mysql://localhost:3306/music?useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC");
         dataSource.setUsername("root");
-        dataSource.setPassword("root");
+        dataSource.setPassword("qwerty");
         return dataSource;
     }
 
